d9d82512d4b782c5af40a94baecd0514a4b4f129
kamrankamrani <kamrankamrani.fard@yahoo.com>
2022-11-03 15:09:39 +0330

    deployed to raspberry and robust built

--- src/Components/ProductTitle/ProductTitle.tsx
+++ src/Components/ProductTitle/ProductTitle.tsx
@@ -7,11 +7,18 @@ export default function ProductTitle() {
   const webSocketData: ScaleResponseType = useAppSelector(
     (state) => state.product
   );
+
+  function decode_utf8(s: string) {
+    return decodeURIComponent(escape(s));
+  }
+
   return (
     <>
       <div className="text-container">
         <p className="title">نام محصول: </p>
-        <p>لیمو سنگی</p>
+        <p>
+          {webSocketData.title && decode_utf8(decodeURI(webSocketData.title))}
+        </p>
       </div>
       <div className="text-container">
         <p className="weight">وزن : </p>
